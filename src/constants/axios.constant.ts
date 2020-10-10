eee7c6b7f03a07f60bfa93096ac841a226aeb9e1
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-10-09 21:42:34 -0700

    openweather to http, so netlify works

--- src/constants/axios.constant.ts
+++ src/constants/axios.constant.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const weatherApiAxios = axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5/",
+  baseURL: "https://api.openweathermap.org/data/2.5/",
 });
 
 export const googlePlacesApiAxios = axios.create({
